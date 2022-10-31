5803c158197e44a592d2a41f9fb757b194e6c658
Mad Nav <72104442+mn51@users.noreply.github.com>
2022-10-31 11:29:55 -0500

    Added robust joint network inference

--- jointinf/methods.py
+++ jointinf/methods.py
@@ -731,6 +731,345 @@ def stat_mod2(zeta_X,
 
 # ##############################################################################
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# sparse_stat_mod3
+@jit(nopython=True)
+def in_wrapper_sparse_stat_mod3(zeta, X,
+                                theta=10,rho1=10,rho2=10,rho3=10,phi=10,eta=1e-1,lmbda=1,Lmbda=10,
+                                eps_abs=0.,eps_rel=1e-3,
+                                num_bins=5,pg_step_size=.01,pg_stop=1e-2,
+                                num_admm_iters=1000,num_subadmm_iters=200,num_pg_iters=200,
+                                G=None
+                                ):
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Graph parameters:
+    K =  len(X)
+    N =  np.array([X[k].shape[0] for k in range(K)])
+    RK = np.array([X[k].shape[1] for k in range(K)])
+    L = np.array([int(N[k]*(N[k]-1)/2) for k in range(K)])
+
+    J = int(G*(G-1)/2)
+
+    low_tri_ind_list=[]
+    L_ind_list=[]
+    U_ind_list=[]
+    D_ind_list=[]
+    for k in range(K):
+        low_tri_ind_list.append(np.where(np.triu(np.ones((N[k],N[k])))-np.eye(N[k])))
+        L_ind_list.append(low_tri_ind_list[k][0]*N[k] + low_tri_ind_list[k][1])
+        U_ind_list.append(low_tri_ind_list[k][1]*N[k] + low_tri_ind_list[k][0])
+        D_ind_list.append(np.arange(N[k])*(N[k]+1))
+
+    low_tri_ind_W = np.where(np.triu(np.ones((G,G)))-np.eye(G))
+    L_ind_W = low_tri_ind_W[0]*G + low_tri_ind_W[1]
+    U_ind_W = low_tri_ind_W[1]*G + low_tri_ind_W[0]
+    D_ind_W = np.arange(G)*(G+1)
+
+    L_ind = np.zeros(np.sum(L))
+    U_ind = np.zeros(np.sum(L))
+    for k in range(K):
+        L_ind[np.sum(L[:k]):np.sum(L[:k])+L[k]] = L_ind_list[k] + np.sum(N[:k]**2)
+        U_ind[np.sum(L[:k]):np.sum(L[:k])+L[k]] = U_ind_list[k] + np.sum(N[:k]**2)
+
+    # Differential matrix for graphon
+    D1=np.concatenate((-np.eye(G-1),np.zeros((1,G-1))),axis=0)+np.concatenate((np.zeros((1,G-1)),np.eye(G-1)),axis=0)
+    D2=np.concatenate((np.eye(G-2),np.zeros((2,G-2))),axis=0)+np.concatenate((np.zeros((1,G-2)),np.concatenate((-2*np.eye(G-2),np.zeros((1,G-2))),axis=0)),axis=0)+np.concatenate((np.zeros((2,G-2)),np.eye(G-2)),axis=0)
+
+    d2_D = np.kron(np.eye(G),D2.T)[:,D_ind_W]
+    d2_L = np.kron(np.eye(G),D2.T)[:,L_ind_W]+np.kron(np.eye(G),D2.T)[:,U_ind_W]
+    d2 = np.zeros((d2_D.shape[0],G+J))
+    d2[:,:G] = d2_D
+    d2[:,G:] = d2_L
+    d1_D = np.kron(D1.T,D1.T)[:,D_ind_W]
+    d1_L = np.kron(D1.T,D1.T)[:,L_ind_W]+np.kron(D1.T,D1.T)[:,U_ind_W]
+    d1 = np.zeros((d1_D.shape[0],G+J))
+    d1[:,:G] = d1_D
+    d1[:,G:] = d1_L
+    # d2 = np.kron(np.eye(G),D2.T)[:,L_ind_W]+np.kron(np.eye(G),D2.T)[:,U_ind_W]
+    # d1 =np.kron(D1.T,D1.T)[:,L_ind_W]+np.kron(D1.T,D1.T)[:,U_ind_W]
+
+    # Network histogram matrix for graphs
+    f=num_bins
+    F=[]
+    for k in range(K):
+        F.append(np.kron(np.eye(f),np.ones((int(np.ceil(N[k]/f)),int(np.ceil(N[k]/f))))-np.eye(int(np.ceil(N[k]/f))))[:int(N[k]),:int(N[k])])
+        F[-1][np.sum(F[-1],axis=0)>0,:]/=(np.sum(F[-1],axis=0)[np.sum(F[-1],axis=0)>0]).reshape(-1,1)
+
+    f_k = [(mykron(F[k],F[k])[:,L_ind_list[k]] + mykron(F[k],F[k])[:,U_ind_list[k]])[L_ind_list[k]] for k in range(K)]
+    f = np.zeros((np.sum(L),np.sum(L)))
+    for k in range(K):
+        f[np.sum(L[:k]):np.sum(L[:k])+L[k],np.sum(L[:k]):np.sum(L[:k])+L[k]] = f_k[k]
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Graphon sampled points zeta
+
+    z_st=[]
+    for k in range(K):
+        z_st.append(np.arange(int(N[k])))
+        for z_idx in range(int(N[k])):
+            z_st[k][z_idx] = int(zeta[k][z_idx]*G)
+        z_st[k][z_st[k]>=G]=G-1
+    z_orig=z_st.copy()
+
+    I_z_st = [np.eye(G)[z_st[k],:] for k in range(K)]
+    Sigma_L = [(np.kron(I_z_st[k],I_z_st[k])[:,L_ind_W] + np.kron(I_z_st[k],I_z_st[k])[:,U_ind_W])[L_ind_list[k]] for k in range(K)]
+    Sigma_D = [(np.kron(I_z_st[k],I_z_st[k])[:,D_ind_W])[L_ind_list[k]] for k in range(K)]
+    Sigma = np.zeros((np.sum(L),G+J))
+    for k in range(K):
+        Sigma[np.sum(L[:k]):np.sum(L[:k])+L[k],:G] = Sigma_D[k]
+        Sigma[np.sum(L[:k]):np.sum(L[:k])+L[k],G:] = Sigma_L[k]
+    # i_k = [(np.kron(I_z_st[k],I_z_st[k])[:,L_ind_W] + np.kron(I_z_st[k],I_z_st[k])[:,U_ind_W])[L_ind_list[k]] for k in range(K)]
+    # Sigma = np.zeros((np.sum(L),J))
+    # for k in range(K):
+    #     Sigma[np.sum(L[:k]):np.sum(L[:k])+L[k],:] = i_k[k]
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    C = []
+    [C.append((X[k]@X[k].T)/RK[k]) for k in range(K)]
+    M_FULL = np.zeros((np.sum(N**2),np.sum(N**2)))
+    for k in range(K):
+        M_FULL[
+              np.sum((N**2)[:k]):np.sum((N**2)[:k])+(N**2)[k],
+              np.sum((N**2)[:k]):np.sum((N**2)[:k])+(N**2)[k]
+              ] \
+            += np.kron(-C[k],np.eye(N[k])) + np.kron(np.eye(N[k]), C[k])
+    M = np.zeros((np.sum(N**2),np.sum(L)))
+    for kl in range(np.sum(L)):
+        M[:,kl] = M_FULL[:,int(L_ind[kl])]+M_FULL[:,int(U_ind[kl])]
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    MAX_ETA = np.min([4,int(np.min(N)/3)])
+    Q = np.zeros((K,np.sum(L)))
+    for k in range(K):
+        Q[k,np.sum(L[:k]):np.sum(L[:k])+L[k]]=1
+
+    s_st = np.random.binomial(1,.5,np.sum(L))*1.
+    p_st = s_st.copy()
+    q_st = Q@s_st-1
+    w_st = np.random.random(G+J)
+    v_st = w_st.copy()
+    t_st = .5*(f@s_st + Sigma@w_st)
+    zeta_st = [z_st[k]/(G-1) for k in range(K)]
+    zeta_orig = zeta_st.copy()
+    u1 = np.zeros(np.sum(L))
+    u2 = np.zeros(K)
+    u3 = np.zeros(G+J)
+
+    pr=[]
+    dr=[]
+    eps_pri=[]
+    eps_dua=[]
+
+    def soft_thresh(z, a):
+        return (z-a)*(z-a > 0) - (-z-a)*(-z-a > 0)
+
+    Phi_s1 = np.linalg.inv( theta*M.T@M + (rho1+phi)*np.eye(np.sum(L)) + rho2*Q.T@Q + 2*Lmbda*f.T@f )
+    # Phi_w1 = np.linalg.inv( 4*lmbda*d2.T@d2 + 4*lmbda*d1.T@d1 + Lmbda*Sigma.T@Sigma + rho3*np.eye(G+J) )
+    Phi_w1 = 4*lmbda*d2.T@d2 + 4*lmbda*d1.T@d1 + Lmbda*Sigma.T@Sigma + rho3*np.eye(G+J)
+
+    for admm_iter in range(num_admm_iters):
+        # s-update
+        s1 = s_st.copy()
+        s2 = s_st.copy()
+        v = np.zeros(np.sum(L))
+        gamma = np.zeros(np.sum(L))
+        gamma[(t_st>0)*(t_st<1)] = np.log(t_st[(t_st>0)*(t_st<1)]/(1-t_st[(t_st>0)*(t_st<1)]))
+
+        pr2 = []
+        dr2 = []
+        eps_pri2 = []
+        eps_dua2 = []
+        for admm_iter2 in range(num_subadmm_iters):
+            s1 = soft_thresh(s2-v,2*eta/phi)
+            Phi_s2 = rho1*(p_st-u1) + rho2*Q.T@(q_st+1-u2) + phi*(s1+v) + gamma + 2*Lmbda*f.T@t_st
+            s2_last = s2.copy()
+            s2 = Phi_s1@Phi_s2
+            v = v + s1 - s2
+
+            pr2.append(np.linalg.norm(s1-s2))
+            dr2.append(np.linalg.norm(phi*(s2-s2_last)))
+            eps_pri2.append( eps_abs*len(s1) + eps_rel*np.amax(np.array([np.linalg.norm(s1),np.linalg.norm(s2)])) )
+            eps_dua2.append( eps_abs*len(s1) + eps_rel*np.linalg.norm(phi*v) )
+            if (
+                (np.sum(np.array(pr2)<=np.array(eps_pri2))>0) and
+                (np.sum(np.array(dr2)<=np.array(eps_dua2))>0)
+                ):
+                break
+
+        s_st = s2.copy()
+
+        # p-update
+        p_last=p_st.copy()
+        ret = s_st + u1
+        p_st = (ret-np.min(ret))/np.max((ret-np.min(ret)))
+        p_st[p_st >= .5] = 1
+        p_st[p_st <  .5] = 0
+
+        # q-update
+        q_last=q_st.copy()
+        q_st = Q@s_st - 1 + u2
+        q_st[q_st<0]=0
+
+        # t-update
+        mu=pg_step_size
+        t_hat = t_st.copy()
+        t_hat[t_hat>1.]=1.
+        t_hat[t_hat<0.]=0.
+        ind_t_hat=(t_hat>0.)*(t_hat<1.)
+        Update_Indices = np.ones(np.sum(L))==1
+        for pg_iter in range(num_pg_iters):
+            last_t=t_hat.copy()
+
+            t_tild = last_t.copy()
+            t_tild[ind_t_hat] = last_t[ind_t_hat] - mu*(s_st[ind_t_hat]-last_t[ind_t_hat])/(last_t[ind_t_hat]*(last_t[ind_t_hat]-1))
+            t_hat = (1/(4*Lmbda+1/mu))*( 2*Lmbda*(f@s_st + Sigma@w_st) + (1/mu)*t_tild )
+            t_hat[t_hat>1]=1
+            t_hat[t_hat<0]=0
+            ind_t_hat=(t_hat>0)*(t_hat<1)
+
+            if np.linalg.norm(last_t-t_hat)**2<=pg_stop:
+                break
+        t_st = t_hat.copy()
+
+        T_st = [lowtri2mat(t_st[k]) for k in range(K)]
+
+        # w-update
+        Phi_w2 = Lmbda*Sigma.T@t_st + rho3*(v_st - u3)
+        # w_st = Phi_w1@Phi_w2
+        w_st = sp.linalg.cho_solve(sp.linalg.cho_factor(Phi_w1),Phi_w2)
+
+        W_st = np.zeros((G,G))
+        W_st[np.eye(G)==1] = w_st[:G]
+        W_st += lowtri2mat(w_st[G:])
+
+        # v-update
+        v_last = v_st.copy()
+        v_st = w_st + u3
+        v_st[v_st>1]=1
+        v_st[v_st<0]=0
+
+        # z-update
+        for k in range(K):
+            eta = np.zeros(N[k])
+            for i in range(N[k]):
+                ETA_RANGE = np.arange(np.max([-z_orig[k][i],-MAX_ETA]),np.min([G-1-z_orig[k][i],MAX_ETA])+1)
+                eta[i] = ETA_RANGE[np.argmin([np.linalg.norm(T_st[k] - \
+                              np.eye(G)[(z_orig[k] + eta_i*np.eye(N[k])[i,:]).astype(int),:]@\
+                              W_st@\
+                              np.eye(G)[:,(z_orig[k] + eta_i*np.eye(N[k])[i,:]).astype(int)]\
+                              )**2 for eta_i in ETA_RANGE])]
+                z_st[k][i]=z_orig[k][i]+eta[i]
+        I_z_st = [np.eye(G)[z_st[k],:] for k in range(K)]
+        Sigma_L = [(np.kron(I_z_st[k],I_z_st[k])[:,L_ind_W] + np.kron(I_z_st[k],I_z_st[k])[:,U_ind_W])[L_ind_list[k]] for k in range(K)]
+        Sigma_D = [(np.kron(I_z_st[k],I_z_st[k])[:,D_ind_W])[L_ind_list[k]] for k in range(K)]
+        Sigma = np.zeros((np.sum(L),G+J))
+        for k in range(K):
+            Sigma[np.sum(L[:k]):np.sum(L[:k])+L[k],:G] = Sigma_D[k]
+            Sigma[np.sum(L[:k]):np.sum(L[:k])+L[k],G:] = Sigma_L[k]
+        Phi_w1 = 4*lmbda*d2.T@d2 + 4*lmbda*d1.T@d1 + Lmbda*Sigma.T@Sigma + rho3*np.eye(G+J)
+        zeta_st=[]
+        for k in range(K):
+            zeta_st.append(z_st[k]/(G-1))
+
+
+        # u-update
+        u1 = u1 + s_st - p_st
+        u2 = u2 + Q@s_st - q_st - 1
+        u3 = u3 + w_st - v_st
+
+        pr.append(
+            np.linalg.norm(s_st-p_st,2) +
+            np.linalg.norm(Q@s_st-q_st-1,2) +
+            np.linalg.norm(w_st-v_st,2)
+            )
+        dr.append(
+            np.linalg.norm(rho1*(p_st-p_last),2) +
+            np.linalg.norm(rho2*Q.T@(q_st-q_last),2) +
+            np.linalg.norm(rho3*(v_st-v_last),2)
+            )
+        eps_pri.append(
+            eps_abs*np.sqrt(len(s_st)) + eps_rel*np.amax( np.array([np.linalg.norm(s_st),np.linalg.norm(p_st)]) ) +
+            eps_abs*np.sqrt(len(s_st)) + eps_rel*np.amax( np.array([(np.linalg.norm(Q@s_st)),np.linalg.norm(q_st),np.linalg.norm(np.ones(K))]) ) +
+            eps_abs*np.sqrt(len(w_st)) + eps_rel*np.amax( np.array([(np.linalg.norm(w_st)),np.linalg.norm(v_st)]) )
+            )
+        eps_dua.append(
+            eps_abs*np.sqrt(len(u1)) + eps_rel*(np.linalg.norm(rho1*u1)) +
+            eps_abs*np.sqrt(len(u2)) + eps_rel*(np.linalg.norm(rho2*Q.T@u2)) +
+            eps_abs*np.sqrt(len(u3)) + eps_rel*(np.linalg.norm(rho3*u3))
+            )
+
+        if (
+            (np.sum(np.array(pr)<=np.array(eps_pri))>0) and
+            (np.sum(np.array(dr)<=np.array(eps_dua))>0)
+            ):
+            break
+
+    s_ret = []
+    t_ret = []
+    for k in range(K):
+        s_ret.append(p_st[np.sum(L[:k]):np.sum(L[:k])+L[k]])
+        t_ret.append(t_st[np.sum(L[:k]):np.sum(L[:k])+L[k]])
+
+    return s_ret, t_ret, v_st, z_st
+
+def sparse_stat_mod3(zeta_X,
+                     theta=10,rho1=10,rho2=10,rho3=10,phi=10,eta=1e-1,lmbda=1,Lmbda=10,
+                     eps_abs=0.,eps_rel=1e-3,
+                     num_bins=5,pg_step_size=.01,pg_stop=1e-2,
+                     num_admm_iters=1000,num_subadmm_iters=200,num_pg_iters=200,
+                     G=None
+                     ):
+    K = len(zeta_X[0])
+    N =  np.array([zeta_X[1][k].shape[0] for k in range(K)])
+    if G is None:
+        G = int(np.sum(N)+10)
+
+    zeta_NUMBA = List()
+    [zeta_NUMBA.append(zeta_X[0][k]) for k in range(K)]
+
+    X_NUMBA = List()
+    [X_NUMBA.append(zeta_X[1][k]) for k in range(K)]
+
+    s_est, t_est, w_est, z_est = in_wrapper_sparse_stat_mod3(zeta_NUMBA,X_NUMBA,
+                                                      theta=theta,rho1=rho1,rho2=rho2,rho3=rho3,phi=phi,eta=eta,lmbda=lmbda,Lmbda=Lmbda,
+                                                      eps_abs=eps_abs,eps_rel=eps_rel,
+                                                      num_bins=num_bins,pg_step_size=pg_step_size,pg_stop=pg_stop,
+                                                      num_admm_iters=num_admm_iters,num_subadmm_iters=num_subadmm_iters,num_pg_iters=num_pg_iters,
+                                                      G=G
+                                                      )
+    for k in range(K):
+        s_est[k] = s_est[k].astype(int)
+    return s_est, t_est, w_est, z_est
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ##############################################################################
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # sparse_stat_mod1
 @jit(nopython=True)
 def in_wrapper_sparse_stat_mod1(X,
